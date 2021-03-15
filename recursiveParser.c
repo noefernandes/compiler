6d9eeb4b873822f606aa16b06bf12ef8f97bd9fa
DanielM08 <danielmarx08@gmail.com>
2021-03-15 09:26:00 -0300

    Atualizando nome dos tokens

--- recursiveParser.c
+++ recursiveParser.c
@@ -157,7 +157,7 @@ void LacoPara(){
             accept(parenteses_esquerda); 
             TipoNumerico(); 
             accept(id); 
-            accept(op_assign); 
+            accept(assign); 
             Expressao(); 
             accept(';'); 
             Expressao(); 
@@ -218,7 +218,7 @@ void Expressao() {
 
 void Exp1() {
     switch(tok) {
-        case op_log_or: accept(op_log_or); TLogico(); Exp1(); break;
+        case op_or: accept(op_or); TLogico(); Exp1(); break;
         case parenteses_direita: break;
         case ']': break;
         case ';': break;
@@ -243,10 +243,10 @@ void TLogico() {
 
 void TLogico1() {
     switch(tok) {
-        case op_log_and: accept(op_log_and); FLogico(); TLogico1(); break;
+        case op_and: accept(op_and); FLogico(); TLogico1(); break;
         case parenteses_direita: break;
         case ']': break;
-        case op_log_or: break;
+        case op_or: break;
         case ';': break;
         case virgula: break;
         default: error("esperando &&, ), ], ||, ;, ou ,\n");
@@ -282,16 +282,16 @@ void ExpRel() {
 
 void ExpRel2() {
     switch(tok) {
-        case op_log_equal: accept(op_log_equal); ExpA(); break;
-        case op_log_diff: accept(op_log_diff); ExpA(); break;
+        case op_equal: accept(op_equal); ExpA(); break;
+        case op_diff: accept(op_diff); ExpA(); break;
         case '<': accept('<'); ExpA(); break;
         case '>': accept('>'); ExpA(); break;
-        case op_log_lte: accept(op_log_lte); ExpA(); break;
-        case op_log_gte: accept(op_log_gte); ExpA(); break;
+        case op_lte: accept(op_lte); ExpA(); break;
+        case op_gte: accept(op_gte); ExpA(); break;
         case parenteses_direita: break;
         case ']': break;
-        case op_log_and: break;
-        case op_log_or: break;
+        case op_and: break;
+        case op_or: break;
         case ';': break;
         case virgula: break;
         default: error("ExpRel2\n");
@@ -315,18 +315,18 @@ void ExpA1() {
     switch(tok) {
         case op_sub: accept(op_sub); Termo(); ExpA1(); break;
         case op_sum: accept(op_sum); Termo(); ExpA1(); break;
-        case op_conj_intersection: accept(op_conj_intersection); Termo(); ExpA1(); break;
-        case op_conj_union: accept(op_conj_union); Termo(); ExpA1(); break;
+        case set_intersection: accept(set_intersection); Termo(); ExpA1(); break;
+        case set_union: accept(set_union); Termo(); ExpA1(); break;
         case parenteses_direita: break;
         case ']': break;
-        case op_log_gte: break;
-        case op_log_lte: break;
+        case op_gte: break;
+        case op_lte: break;
         case '<': break;
         case '>': break;
-        case op_log_diff: break;
-        case op_log_equal: break;
-        case op_log_and: break;
-        case op_log_or: break;
+        case op_diff: break;
+        case op_equal: break;
+        case op_and: break;
+        case op_or: break;
         case ';': break;
         case virgula: break;
         default: error("ExpA1\n");
@@ -355,16 +355,16 @@ void Termo1() {
         case ']': break;
         case op_sub: break;
         case op_sum: break;
-        case op_conj_intersection: break;
-        case op_conj_union: break;
-        case op_log_gte: break;
-        case op_log_lte: break;
+        case set_intersection: break;
+        case set_union: break;
+        case op_gte: break;
+        case op_lte: break;
         case '<': break;
         case '>': break;
-        case op_log_diff: break;
-        case op_log_equal: break;
-        case op_log_and: break;
-        case op_log_or: break;
+        case op_diff: break;
+        case op_equal: break;
+        case op_and: break;
+        case op_or: break;
         case ';': break;
         case virgula: break;
         default: error("Termo1\n");
@@ -394,16 +394,16 @@ void Fator2() {
         case op_mod: break;
         case op_div: break;
         case op_mult: break;
-        case op_conj_intersection: break;
-        case op_conj_union: break;
-        case op_log_gte: break;
-        case op_log_lte: break;
+        case set_intersection: break;
+        case set_union: break;
+        case op_gte: break;
+        case op_lte: break;
         case '<': break;
         case '>': break;
-        case op_log_diff: break;
-        case op_log_equal: break;
-        case op_log_and: break;
-        case op_log_or: break;
+        case op_diff: break;
+        case op_equal: break;
+        case op_and: break;
+        case op_or: break;
         case ';': break;
         case virgula: break;
         default: error("Fator2\n");
